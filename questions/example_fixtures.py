318a6e1f99fd4bc9dd8f5e2f19046522b2b29dd1
Lee Penkman <leepenkman@gmail.com>
2023-07-02 12:33:07 +1200

    fix qna

--- questions/example_fixtures.py
+++ questions/example_fixtures.py
@@ -542,6 +542,7 @@ openai_examples = {
     "default-factual-answering": {
         "id": "default-factual-answering",
         "title": "Factual answering",
+        "description": "answering factually about questions",
         "tags": ["Answers", "Generation", "Conversation", "Classification"],
         "generate_params": {
             "text": "Q: Who is Batman?\nA: Batman is a fictional comic book character.\n\nQ: What is torsalplexity?\nA: ?\n\nQ: What is Devz9?\nA: ?\n\nQ: Who is George Lucas?\nA: George Lucas is American film director and producer famous for creating Star Wars.\n\nQ: What is the capital of California?\nA: Sacramento.\n\nQ: What orbits the Earth?\nA: The Moon.\n\nQ: Who is Fred Rickerson?\nA: ?\n\nQ: What is an atom?\nA: An atom is a tiny particle that makes up everything.\n\nQ: Who is Alvan Muntz?\nA: ?\n\nQ: What is Kozar-09?\nA: ?\n\nQ: How many moons does Mars have?\nA: Two, Phobos and Deimos.\n\nQ: What's a language model?\nA:",
