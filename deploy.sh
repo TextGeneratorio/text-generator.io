0892be29955c6d268f45b5545a9091cef7372348
Lee Penkman <leepenkman@gmail.com>
2024-05-09 19:08:49 +1200

    pip install first? todo need ci as this is dumb

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+# validate we can install requirements
+pip install -r requirements.txt
+echo "WARNING: Run tests before deploying"
 gsutil -m rsync -r ./static gs://static.text-generator.io/static
 gcloud app deploy --project questions-346919
 
