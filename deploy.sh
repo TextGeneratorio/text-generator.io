c4d4a47e939b135bf4faa99ca01fbd4940ddeea2
Lee Penkman <leepenkman@gmail.com>
2025-01-23 17:53:39 +1300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-set -e
-set -x
-set -o pipefail
+#set -e
+#set -x
+#set -o pipefail
 
 # Set the project ID
 PROJECT_ID="questions-346919"
@@ -12,8 +12,8 @@ gcloud config set project $PROJECT_ID
 echo "Current project set to: $(gcloud config get-value project)"
 
 # validate we can install requirements
-pip install -r requirements.txt
-echo "WARNING: Run tests before deploying"
+# uv pip install -r requirements.txt
+echo "WARNING: Run tests+pip install before deploying"
 gsutil -m rsync -r ./static gs://static.text-generator.io/static
 gcloud app deploy --project questions-346919
 
