73cfee56470612adf468baf15321be14615454ef
Lee Penkman <leepenkman@gmail.com>
2025-07-11 13:40:53 +1200

    fix

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,22 @@
 #set -o pipefail
 
 # Set the project ID
-PROJECT_ID="questions-346919"
+#PROJECT_ID="questions-346919"
 
 # Use the specified project
-gcloud config set project $PROJECT_ID
+#gcloud config set project $PROJECT_ID
 
 # Verify the current project
-echo "Current project set to: $(gcloud config get-value project)"
+#echo "Current project set to: $(gcloud config get-value project)"
 
 # validate we can install requirements
 # uv pip install -r requirements.txt
 echo "WARNING: Run tests+pip install before deploying"
-gsutil -m rsync -r ./static gs://static.text-generator.io/static
-gcloud app deploy --project questions-346919
+
+# Sync static files to Cloudflare R2 bucket
+aws s3 sync ./static s3://text-generatorstatic/static --endpoint-url https://f76d25b8b86cfa5638f43016510d8f77.r2.cloudflarestorage.com
+
+#gcloud app deploy --project questions-346919
 
 
 
